4831dd77f1d9e92f218cac62842965fef835d909
lyx <>
2021-11-16 10:42:40 +0800

    小程序基础接口及环境判断

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,12 +4,13 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
+    motto: 'Hello World cc',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName'), // 如需尝试获取用户信息可改为false
+    environment: "环境未知",
   },
   // 事件处理函数
   bindViewTap() {
@@ -18,11 +19,66 @@ Page({
     })
   },
   onLoad() {
+
     if (wx.getUserProfile) {
       this.setData({
         canIUseGetUserProfile: true
       })
     }
+  },
+
+  version(){
+    wx.qy.getSystemInfo({
+      success (res) {
+        console.log("version"+res.version)
+      }
+    })
+
+  },
+  qyuse(){
+    console.log("Rwx.qy.getNFCReaderState:"+wx.qy.canIUse('getNFCReaderState'));
+    // console.log("Rwx.qy.shareToTrainee:"+wx.canIUse('wx.qy.shareToTrainee'));
+  },
+  check(){
+    let _this = this
+    wx.getSystemInfo({
+      success (res) {
+        console.log(res.model)
+        console.log(res.pixelRatio)
+        console.log(res.windowWidth)
+        console.log(res.windowHeight)
+        console.log(res.language)
+        console.log(res.version)
+        console.log(res.platform)
+        console.log(res.environment)
+        if(res.environment &&  res.environment=="wxwork"){
+          _this.setData({
+            environment: "当前为企业微信环境"
+          })
+          _this.version();
+          _this.qyuse();
+        }else{
+          _this.setData({
+            environment: "当前为微信环境"
+          })
+        }
+      }
+    })
+  },
+  test(){
+    let _this = this
+    wx.request({
+      url: 'http://tobdev.ant-xy.com:9900/test', //仅为示例，并非真实的接口地址
+      success (res) {
+        wx.showToast({
+          title: res.data.data.data,
+        })
+        _this.setData({
+          motto: res.data.data.data,
+        })
+        console.log(res.data)
+      }
+    })
   },
   getUserProfile(e) {
     // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
